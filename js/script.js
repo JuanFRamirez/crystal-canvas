5643e0357a1532305e119697b570c4f194d33561
Juan Francisco Ramirez <62062374+JuanFRamirez@users.noreply.github.com>
2022-04-11 19:33:30 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("on");
+  const canvas = document.querySelector("#square");
+  const canvasContext = canvas.getContext("2d");
+
+  const sphere = document.querySelector("#sphere");
+  const sphereContext  = sphere.getContext('2d');
+ 
+});
